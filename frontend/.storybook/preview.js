39d52706ef7facea3fa0b0d19de202962fd72d72
rakeshreddym21 <rakeshreddy.m@zemosolabs.com>
2022-08-05 15:14:55 +0530

    changes

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,6 @@
+import "./storybook.css";
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +10,9 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+      <Story />
+  ),
+];
