ca9e7221ab6bb413bfb9acceca25449d28105227
VanshikaGarg1110 <vanshika.garg@zemosolabs.com>
2022-08-10 11:56:21 +0530

    Removed makeStyles

--- frontend/src/components/molecules/testimonial/index.tsx
+++ frontend/src/components/molecules/testimonial/index.tsx
@@ -1,5 +1,4 @@
 import { Grid, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import React from "react";
 import theme from "../../../theme";
 import Avatar from "../../atoms/Avatar";
@@ -11,60 +10,58 @@ type Props = {
     comment: string;
 }
 
+const style = {
+    root: {
+        backgroundColor: theme.palette.gammaWhite.main,
+        borderRadius: theme.spacing(2),
+        height: "100%",
+        width: "100%",
+    },
+
+    innerGrid: {
+        display: "flex",
+        flexDirection: "column",
+        padding: theme.spacing(5),
+        gap: theme.spacing(2.5),
+    },
+
+    frame: {
+        display: "flex",
+        flexDirection: "row",
+        gap: theme.spacing(2.5),
+    },
+
+    comment: {
+        color: theme.palette.gammaMedium.main,
+        fontWeight: 400,
+        paddingLeft: theme.spacing(13),
+    },
+
+    name: {
+        color: theme.palette.gammaMedium.main,
+    },
+
+    location: {
+        color: theme.palette.gammaLow.main,
+    }
+};
+
 const Testimonial: React.FC<Props> = (props: Props) => {
-    const useStyles = makeStyles({
-        root: {
-            backgroundColor: theme.palette.gammaWhite.main,
-            borderRadius: theme.spacing(2),
-            height: "100%",
-            width: "100%",
-        },
-
-        innerGrid: {
-            display: "flex",
-            flexDirection: "column",
-            padding: theme.spacing(5),
-            gap: theme.spacing(2.5),
-        },
-
-        frame: {
-            display: "flex",
-            flexDirection: "row",
-            gap: theme.spacing(2.5),
-        },
-
-        comment: {
-            color: theme.palette.gammaMedium.main,
-            fontWeight: 400,
-            paddingLeft: theme.spacing(13),
-        },
-
-        name: {
-            color: theme.palette.gammaMedium.main,
-        },
-
-        location: {
-            color: theme.palette.gammaLow.main,
-        }
-    });
-
-    const classes = useStyles();
-
     return (
-        <Grid className={classes.root}>
-            <Grid item container className={classes.innerGrid}>
-                <Grid item className={classes.frame}>
+        <Grid sx={style.root}>
+            <Grid item container sx={style.innerGrid}>
+                <Grid item sx={style.frame}>
                     <Grid item>
                         <Avatar src={props.avatar} alt="avatar"></Avatar>
                     </Grid>
                     <Grid item>
                         <Grid item>
-                            <Typography variant="caption2" className={classes.name}>{props.name}</Typography><br></br>
-                            <Typography variant="overline" className={classes.location}>{props.location}</Typography>
+                            <Typography variant="caption2" sx={style.name}>{props.name}</Typography><br></br>
+                            <Typography variant="overline" sx={style.location}>{props.location}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item className={classes.comment}>
+                <Grid item sx={style.comment}>
                     <Typography variant="caption2">&ldquo;{props.comment}&rdquo;</Typography>
                  </Grid>
             </Grid>
