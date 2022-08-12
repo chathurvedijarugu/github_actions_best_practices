9d92589eba4d66316fc4bd7461ddaad6fcf0114d
rakeshreddym21 <rakeshreddy.m@zemosolabs.com>
2022-08-12 21:15:17 +0530

    added userprofile molecule

--- frontend/src/components/molecules/UserProfileMolecule/index.stories.tsx
+++ frontend/src/components/molecules/UserProfileMolecule/index.stories.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import UserProfileMolecule from ".";
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import UserProfileMolecule from '.'
+import { Dialog } from '@mui/material'
 
 export default {
-  title: "Molecules/UserProfileMolecule",
+  title: 'Molecules/UserProfileMolecule',
   component: UserProfileMolecule,
-} as ComponentMeta<typeof UserProfileMolecule>;
+} as ComponentMeta<typeof UserProfileMolecule>
 
-const Template: ComponentStory<typeof UserProfileMolecule> = (args) => <UserProfileMolecule />;
+const Template: ComponentStory<typeof UserProfileMolecule> = (args) => (
+  <Dialog
+    scroll="body"
+    PaperProps={{ sx: { position: 'fixed', top: 10, right: 10, m: 0 } }}
+    maxWidth={false}
+    open={true}
+  >
+    <UserProfileMolecule {...args} />
+  </Dialog>
+)
 
-export const UserProfile = Template.bind({});
+export const UserProfile = Template.bind({})
+UserProfile.args = {
+  handleClose: () => console.log('Icon Closed Clicked'),
+  handleLogOut: () => console.log('Logout Clicked'),
+}
--- frontend/src/components/molecules/UserProfileMolecule/index.tsx
+++ frontend/src/components/molecules/UserProfileMolecule/index.tsx
@@ -2,17 +2,15 @@ import { Box, Grid, Typography } from '@mui/material'
 import React from 'react'
 import theme from '../../../theme'
 import { profileMenu } from '../../../utils/constant'
-import { text } from '../../atoms/Button/index.stories'
 import UserProfileCard from '../UserProfileCard'
 import profilePic from '../../../assets/userprofileIcons/Avatar.svg'
+import closeIcon from '../../../assets/userprofileIcons/closeIcon.svg'
+
 const stylings = {
   box: {
     width: '15.438rem',
     height: '5.125rem',
-    // border: '1px solid #F8F8FA',
-    // boxShadow: '0px 0px 12px rgba(233, 232, 237, 0.53)',
-    // borderRadius: '12px',
-    paddingLeft:"7.688rem"
+      paddingLeft: '7.688rem',
   },
   innerBox: {
     display: 'flex',
@@ -42,34 +40,71 @@ const stylings = {
     color: theme.palette.gammaLow.main,
   },
 }
-
-const index = () => {
+export type Props = {
+  handleClose: () => void
+  handleLogOut: () => void
+}
+const index = (props: Props) => {
   return (
-    <>
-      <Box sx={stylings.box}>
-        <Grid container sx={stylings.container}>
-          <Grid item data-testid="illustrationImage">
-            <img src={profilePic} alt="Text" />
-          </Grid>
-          <Grid item>
-            <Box sx={stylings.innerBox}>
-              <Typography variant="body" sx={stylings.text1}>
-                Patrick Smith
-              </Typography>
-              <Typography variant="overline" sx={stylings.text3}>
-                patricksmith@gmail.com{' '}
-              </Typography>
-              <Typography variant="overline" sx={stylings.text3}>
-                + 1 330-617-22334
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
+    <Box
+      sx={{
+        paddingLeft: '3.75rem',
+        paddingRight: '3.75rem',
+        paddingTop: '2.5rem',
+        paddingBottom: '7rem',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row-reverse',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '590px',
+        }}
+      >
+        <img onClick={props.handleClose} src={closeIcon} alt="closeicon"></img>
       </Box>
-      <Grid container direction="column" rowGap="2rem">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '428px',
+        }}
+      >
+        <Box sx={stylings.box}>
+          <Grid container sx={stylings.container}>
+            <Grid item data-testid="illustrationImage">
+              <img src={profilePic} alt="Text" />
+            </Grid>
+            <Grid item>
+              <Box sx={stylings.innerBox}>
+                <Typography variant="body" sx={stylings.text1}>
+                  Patrick Smith
+                </Typography>
+                <Typography variant="overline" sx={stylings.text3}>
+                  patricksmith@gmail.com{' '}
+                </Typography>
+                <Typography variant="overline" sx={stylings.text3}>
+                  + 1 330-617-22334
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+        <Typography variant="body2" color="#6B4DE0">
+          Edit
+        </Typography>
+      </Box>
+      <Grid container direction="column" rowGap="2rem" paddingTop="3rem">
         {profileMenu.map((item: any, index: number) => {
           return (
-            <Grid item>
+            <Grid
+              item
+              onClick={item.heading === 'Logout' ? props.handleLogOut : ()=>null}
+            >
               <UserProfileCard
                 icon={item.icon}
                 heading={item.heading}
@@ -79,7 +114,7 @@ const index = () => {
           )
         })}
       </Grid>
-    </>
+    </Box>
   )
 }
 
