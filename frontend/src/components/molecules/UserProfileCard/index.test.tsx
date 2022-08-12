b4ec85f74f0d0bc2a184d29faaab2dd5477feedc
rakeshreddym21 <rakeshreddy.m@zemosolabs.com>
2022-08-12 12:02:02 +0530

    changes

---
+++ frontend/src/components/molecules/UserProfileCard/index.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import UserProfileCard from "./index";
+import homeImg from '../../../assets/userprofileIcons/home.svg'
+
+describe("UserProfileCard Component", () => {
+  it("rendering component", () => {
+    const wrapper = render(<UserProfileCard icon={homeImg} heading="Manage Address" subtext="" />);
+    expect(wrapper).toBeTruthy;
+  });
+  it("rendering component", () => {
+    const wrapper = render(<UserProfileCard icon={homeImg} heading="Manage Address" subtext="2235 Calfornia" />);
+    expect(wrapper).toBeTruthy;
+  });
+});
