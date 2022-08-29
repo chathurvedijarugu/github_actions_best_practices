477d8ef27ef59ac704c7d4afc475525e6566b663
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-08-29 18:29:09 +0530

    Add:sonar pipeline for appointments-service

--- backend/Service-Registry/src/main/java/com/zemoso/ServiceRegistry/ServiceRegistryApplication.java
+++ backend/Service-Registry/src/main/java/com/zemoso/ServiceRegistry/ServiceRegistryApplication.java
@@ -3,6 +3,7 @@ package com.zemoso.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
