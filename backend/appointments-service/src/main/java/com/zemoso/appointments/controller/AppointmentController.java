b37f580a9b897e97e378d232b4db98e64e1a0d0f
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-08-31 23:27:33 +0530

    Add: Appointment-service microservice

---
+++ backend/appointments-service/src/main/java/com/zemoso/appointments/controller/AppointmentController.java
@@ -0,0 +1,28 @@
+package com.zemoso.appointments.controller;
+
+import com.zemoso.appointments.dto.AppointmentDto;
+import com.zemoso.appointments.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/appointements")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @GetMapping("/")
+    @ResponseBody
+    public List<AppointmentDto> findAll(){
+        return appointmentService.findAll();
+    }
+    @PostMapping("/")
+    @ResponseBody
+    public AppointmentDto saveAppointment(@RequestBody AppointmentDto appointmentDto){
+        appointmentService.saveAppointment(appointmentDto);
+        return appointmentDto;
+    }
+}
