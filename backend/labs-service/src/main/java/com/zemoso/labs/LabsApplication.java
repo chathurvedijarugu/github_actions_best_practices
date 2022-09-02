e0f855707237b80e9595a39d8f8a10ea2a8010a5
rakeshreddym21 <98313966+rakeshreddym21@users.noreply.github.com>
2022-09-02 14:50:23 +0530

    BE b58 zd 80 ap is for labs (#96)
    * Added Labs Microservice

--- backend/labs-service/src/main/java/com/zemoso/labs/LabsApplication.java
+++ backend/labs-service/src/main/java/com/zemoso/labs/LabsApplication.java
@@ -2,8 +2,10 @@ package com.zemoso.labs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LabsApplication {
 
 	public static void main(String[] args) {
---
+++ backend/labs-service/src/main/java/com/zemoso/labs/mapper/LabsMapper.java
@@ -0,0 +1,19 @@
+package com.zemoso.labs.mapper;
+import com.zemoso.labs.dto.LabsDto;
+import com.zemoso.labs.entity.Labs;
+import org.mapstruct.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LabsMapper {
+    private final ModelMapper mapper = new ModelMapper();
+
+   public   LabsDto labsToLabsDto(Labs labs){
+       return mapper.map(labs,LabsDto.class);
+   }
+    public Labs labsDtoToLabs(LabsDto labsDto)
+    {
+        return mapper.map(labsDto,Labs.class);
+    }
+}
